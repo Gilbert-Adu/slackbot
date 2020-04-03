97084895850c5f40c6e2be59dd8327d05011782e
Gilbert Adu <gea33@cornell.edu>
2020-04-03 19:42:31 -0400

    new changes made

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const Slackbot = require('slackbots');
 const axios = require('axios');
 const dotenv  = require('dotenv');
-
+//import dotenv from 'dotenv'
 dotenv.config()
 
+const BOT_TOKEN = process.env.BOT_TOKEN
+
 const bot = new Slackbot({
-    token: `${process.env.BOT_TOKEN}`,
+    token: BOT_TOKEN,
     name: 'gilApp'
 })
